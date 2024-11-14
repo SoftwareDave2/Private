5608b06ec4f47d369e81fba56e0dd80754b56846
MikWink <winklermi76145@th-nuernberg.de>
2024-11-14 16:27:55 +0100

    Initial commit

---
+++ src/main/java/master/it_projekt_tablohm/ItProjektTablohmApplication.java
@@ -0,0 +1,13 @@
+package master.it_projekt_tablohm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItProjektTablohmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItProjektTablohmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/master/it_projekt_tablohm/ItProjektTablohmApplicationTests.java
@@ -0,0 +1,13 @@
+package master.it_projekt_tablohm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItProjektTablohmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
