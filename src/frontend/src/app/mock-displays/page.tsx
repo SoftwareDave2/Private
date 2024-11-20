1be81a88d8fa0d53e9e6834444dfd8929873184b
markus <markus@iMac-Pro.fritz.box>
2024-11-20 15:36:13 +0100

    /mock-users zeigt die erstellten displays jetzt erfolgreich an und über dsas textfeld lässt sich der filename setzen. trotzdem kommt weiterhin der Fehler: "[ Server ] Error: Unexpected token 'S', "Saved" is not valid JSON"
    nach einem reloaden der seite werden die Displays bzw deren id und filepath name angezeigt.

--- src/frontend/src/app/mock-displays/page.tsx
+++ src/frontend/src/app/mock-displays/page.tsx
@@ -1,7 +1,7 @@
 // in dem beispiel mit mockapi.io durchgeführt, aber dafür müsste amn extra einen account machen:
 // Link zum gesmaten YouTube Tutorial: https://www.youtube.com/watch?v=_EgI9WH8q1A
 import {revalidatePath} from "next/cache";
-type MockUser = {
+type Display = {
     id: number;
     brand: string;
     model: string;
@@ -22,7 +22,7 @@ export default async function MockUsers() {
                 headers: {"Content-Type": "application/x-www-form-urlencoded",
                     //Authorization: "Bearer YOUR_PRIVATE_KEY" // secure because it is server side code
                 },
-                body: 'brand=Phillips&model=Tableux&width=1920&height=1080&orientation=vertical&filename=Filename: '+name
+                body: 'brand=Phillips&model=Tableux&width=1920&height=1080&orientation=vertical&filename='+name
             }
         );
         const newUser = await res.json();
@@ -38,15 +38,15 @@ export default async function MockUsers() {
         <div className="py-10">
             <form action = {addUser} className="mb-4">
                 <input type="text" name="name" required className="border p-2 mr-2"/>
-                <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">Add user</button>
+                <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">Add display</button>
 
 
             </form>
 
             <ul className="space-y-4 p-4">
-                {displays.map((display: MockUser) => (
+                {displays.map((display: Display) => (
                     <li key={display.id} className="p-4 bg-white shadow-md rounded-lg text-grey-700">
-                        {display.id} ({display.filename})
+                        Display id: {display.id} , Filepath: ({display.filename})
                     </li>
                 ))}
             </ul>
