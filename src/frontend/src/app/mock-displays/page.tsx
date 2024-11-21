0926d5411e1076d92a8a5711d258389a8da18205
markus <markus@iMac-Pro.fritz.box>
2024-11-21 11:43:57 +0100

    bei /mock-displays eine funktion zum löschen bzw. entfernen der Displays über deren Id hinzugefügt.

--- src/frontend/src/app/mock-displays/page.tsx
+++ src/frontend/src/app/mock-displays/page.tsx
@@ -1,31 +1,49 @@
 // in dem beispiel mit mockapi.io durchgeführt, aber dafür müsste amn extra einen account machen:
 // Link zum gesmaten YouTube Tutorial: https://www.youtube.com/watch?v=_EgI9WH8q1A
 import {revalidatePath} from "next/cache";
+import FormComponentAddDisplay from "@/components/FormComponentAddDisplay";
 type Display = {
     id: number;
     brand: string;
     model: string;
+    width: number;
+    height: number;
+    orientation: string;
     filename: string;
 };
 
-export default async function MockUsers() {
+
+export default async function MockDisplays() {
     //await new Promise((resolve) => setTimeout(resolve, 2000));
     //const response = await fetch("der eigentliche Link, von dem die Liste der user gefetched wird");
     const response = await fetch("http://localhost:8080/display/all");
     const displays = await response.json();
 
-    async function addUser(formdata: FormData){
+    async function addDisplay(formdata: FormData){
         "use server"
-        const name = formdata.get("name");
+        const filename = formdata.get("filename");
         const res = await fetch("http://localhost:8080/display/add", {
                 method: "POST",
                 headers: {"Content-Type": "application/x-www-form-urlencoded",
                     //Authorization: "Bearer YOUR_PRIVATE_KEY" // secure because it is server side code.
                 },
-                body: 'brand=Phillips&model=Tableux&width=1920&height=1080&orientation=vertical&filename='+name
+                body: 'brand=Phillips&model=Tableux&width=1920&height=1080&orientation=vertical&filename='+filename
             }
         );
-        const newUser = await res.json();
+        //const newUser = await res.json();
+        //await res.json();
+        revalidatePath("/mock-displays");
+        //console.log(newUser);
+    }
+
+    async function removeDisplay(formdata: FormData){
+        "use server"
+        const id = formdata.get("id");
+        const res = await fetch("http://localhost:8080/display/delete/"+id, {
+                method: "DELETE"
+            }
+        );
+        //await res.json();
         revalidatePath("/mock-displays");
         //console.log(newUser);
     }
@@ -33,12 +51,17 @@ export default async function MockUsers() {
 
 
 
-
     return (
         <div className="py-10">
-            <form action = {addUser} className="mb-4">
-                <input type="text" name="name" required className="border p-2 mr-2"/>
+            <form action={addDisplay} className="mb-4">
+                <input type="text" name="filename" required className="border p-2 mr-2"/>
                 <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">Add display</button>
+
+
+            </form>
+            <form action={removeDisplay} className="mb-4">
+                <input type="text" name="id" required className="border p-2 mr-2"/>
+                <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">remove display</button>
 
 
             </form>
@@ -51,5 +74,7 @@ export default async function MockUsers() {
                 ))}
             </ul>
         </div>
+
+
     );
 }
--- src/frontend/src/app/page.tsx
+++ src/frontend/src/app/page.tsx
@@ -1,6 +1,8 @@
+"use client"
 import PageHeader from "@/components/PageHeader";
 import DisplayFrame from "@/components/DisplayFrame";
 import Fetch_picture from "@/components/Fetch_picture";
+import FormComponentAddDisplay from "@/components/FormComponentAddDisplay";
 
 export default function Home() {
     let displays = [
@@ -15,13 +17,17 @@ export default function Home() {
     ]
 
     return (
-      <main>
-          <PageHeader title={'Dashboard'} info={'9 Bildschirme laufen - 0 Bildschirme gestoppt'}>
-          </PageHeader>
-          <div className={`flex gap-4 flex-wrap`}>
-              {displays.map(display => <DisplayFrame key={display.name} name={display.name} />)}
-          </div>
-          < Fetch_picture />
-      </main>
-  );
+        <main>
+            <PageHeader title={'Dashboard'} info={'9 Bildschirme laufen - 0 Bildschirme gestoppt'}>
+            </PageHeader>
+            <div className={`flex gap-4 flex-wrap`}>
+                {displays.map(display => <DisplayFrame key={display.name} name={display.name}/>)}
+            </div>
+            <div>
+                <h1>Formular zur Datenübertragung</h1>
+                <FormComponentAddDisplay/>
+            </div>
+            < Fetch_picture/>
+        </main>
+    );
 }
