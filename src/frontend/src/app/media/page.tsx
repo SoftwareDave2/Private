0995c7927addb081d9a921231d34e60bcdbdd5ee
Leonhard Haselbek <haselbekle100200@th-nuernberg.de>
2024-11-27 12:58:07 +0100

    Add page header button component.

--- src/frontend/src/app/media/page.tsx
+++ src/frontend/src/app/media/page.tsx
@@ -1,10 +1,14 @@
+'use client'
+
 import PageHeader from "@/components/PageHeader";
+import PageHeaderButton from "@/components/PageHeaderButton";
+import React from "react";
 
 export default function Media() {
     return (
         <main>
             <PageHeader title={'Mediathek'} info={''}>
-
+                <PageHeaderButton onClick={() => { }}>Hochladen</PageHeaderButton>
             </PageHeader>
         </main>
     )
