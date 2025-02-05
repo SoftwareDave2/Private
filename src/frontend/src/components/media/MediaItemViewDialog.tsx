21d4b2410d6d603840a0fca77565493648c7809f
Leonhard Haselbek <haselbekle100200@th-nuernberg.de>
2025-02-05 21:55:20 +0100

    Show image resolution in media store dialog.

--- src/frontend/src/components/media/MediaItemViewDialog.tsx
+++ src/frontend/src/components/media/MediaItemViewDialog.tsx
@@ -14,6 +14,13 @@ type MediaItemViewDialogProps = {
 export default function MediaItemViewDialog({open, filename, onClose, onDeleted}: MediaItemViewDialogProps) {
 
     const [showConfirmDeleteDialog, setShowConfirmDeleteDialog] = useState(false)
+    const [width, setWidth] = useState<number | undefined>()
+    const [height, setHeight] = useState<number | undefined>()
+
+    const loadedHandler = (imgWidth: number, imgHeight: number) => {
+        setWidth(imgWidth)
+        setHeight(imgHeight)
+    }
 
     const toggleShowConfirmDeleteDialog =
         () => setShowConfirmDeleteDialog(!showConfirmDeleteDialog)
@@ -26,10 +33,11 @@ export default function MediaItemViewDialog({open, filename, onClose, onDeleted}
     return (
         <>
             <Dialog open={open} size={'xl'} handler={onClose} className={'!w-auto !min-w-min'}>
-                <DialogHeader>{filename}</DialogHeader>
+                <DialogHeader>{filename} {width && height && <span className={'ms-2 text-gray-600'}>({width}x{height})</span>}</DialogHeader>
                 <DialogBody>
                     <div className={styles.img}>
-                        <Image filename={filename} className={'rounded-sm w-full h-full object-contain'} />
+                        <Image filename={filename} className={'rounded-sm w-full h-full object-contain'}
+                               onImageLoaded={loadedHandler} />
                     </div>
                     <ConfirmDeleteImageDialog open={showConfirmDeleteDialog} filename={filename}
                                               onClose={toggleShowConfirmDeleteDialog} onDeleted={deletedHandler} />
--- src/frontend/src/components/shared/Image.tsx
+++ src/frontend/src/components/shared/Image.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, {SyntheticEvent} from "react";
 
 type ImageProps = React.ImgHTMLAttributes<HTMLImageElement> & {
-    filename: string
+    filename: string,
+    onImageLoaded?: (width: number, height: number) => void
 }
 
-export default function Image({filename, ...props}: ImageProps) {
+export default function Image({filename, onImageLoaded, ...props}: ImageProps) {
     const imageFolderPath = '/uploads/'
 
+    const loadedHandler = (e: SyntheticEvent<HTMLImageElement, Event>) => {
+        e.preventDefault()
+        const img = e.target as HTMLImageElement
+        if (onImageLoaded) {
+            onImageLoaded(img.naturalWidth, img.naturalHeight)
+        }
+    }
+
     return (
-        <img src={imageFolderPath + filename} alt={''} {...props} />
+        <img src={imageFolderPath + filename} alt={''} {...props} onLoad={loadedHandler} />
     )
 }
