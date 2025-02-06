530f73fb2ff5a5aedbd8ba682db1ee782b4fb364
Leonhard Haselbek <haselbekle100200@th-nuernberg.de>
2025-02-06 12:25:22 +0100

    Refactor display filters on calendar page.

--- src/frontend/src/app/calendar/page.tsx
+++ src/frontend/src/app/calendar/page.tsx
@@ -10,38 +10,34 @@ import {EventDetails} from "@/types/eventDetails";
 import {  useEffect } from 'react';
 import PageHeader from "@/components/layout/PageHeader";
 import {EventClickArg} from "@fullcalendar/core";
-
-
-type Display = {
-    macAddress: string;
-    id: number;
-    brand: string;
-    model: string;
-    width: number;
-    height: number;
-    orientation: string;
-    filename: string;
-    wakeTime: string;
-};
+import DisplayFilters from "@/components/calendar/DisplayFilters";
 
 export default function Calendar() {
+
+    const backendApiUrl = 'http://localhost:8080'
+
     const calendarRef = useRef<FullCalendar | null>(null)
     const hasFetched = useRef(false)
     const [events, setEvents] = useState<EventDetails[]>([])
     const [openCalendarEntry, setOpenCalendarEntry] = useState<boolean>(false)
     const [eventDetailsForEdit, setEventDetailsForEdit] = useState<EventDetails | null>(null)
-
-    const [displays, setDisplays] = useState<Display[]>([]);
     const [selectedMACs, setSelectedMACs] = useState<string[]>([]);
 
+    useEffect(() => {
+        if (hasFetched.current) return
+        hasFetched.current = true
+
+        const savedSelections = getSelectedDisplaysFromStorage()
+        setSelectedMACs(savedSelections)
+
+        updateEvents()
+            .then(() => console.log('Events updated.'))
+            .catch(err => console.error(err))
+    }, [])
+
     const updateEvents = async () => {
-
-        // Lade die ausgewählten MAC-Adressen direkt aus dem localStorage
-        const savedSelections = JSON.parse(localStorage.getItem("selectedMACs") || "[]");
-        console.log("Aktuelle ausgewählte MAC-Adressen:", savedSelections);
-
-        const response = await fetch('http://localhost:8080/event/all');
-        const eventData = (await response.json()) as EventDetails[];
+        const response = await fetch(backendApiUrl + '/event/all')
+        const eventData = (await response.json()) as EventDetails[]
 
         // TODO: Workaroud until multiple displays can be saved for events.
         eventData.forEach(d => {
@@ -51,14 +47,18 @@ export default function Calendar() {
             }]
         })
 
-        // Events filtern basierend auf den ausgewählten MAC-Adressen
-        const filteredEvents = eventData.filter(event => savedSelections.includes(event.display.macAddress));
+        const savedSelections = getSelectedDisplaysFromStorage()
+        const filteredEvents = eventData.filter(event =>
+            savedSelections.includes(event.display.macAddress))
+        setEvents(filteredEvents)
+    }
 
-        console.log("Gefilterte Events:", filteredEvents);
-        setEvents(filteredEvents);
+    const getSelectedDisplaysFromStorage = () =>
+        JSON.parse(localStorage.getItem("selectedMACs") || "[]")
 
-
-    } 
+    const setSelectedDisplaysOnStorage = (macs: string[]) => {
+        localStorage.setItem("selectedMACs", JSON.stringify(macs))
+    }
 
     const formatDateToString = (date: Date, onlyDate: boolean) => {
         const dateStr = date.toLocaleDateString('en-CA')    // returns 1970-01-01
@@ -70,62 +70,11 @@ export default function Calendar() {
         }
     }
 
-    useEffect(() => {
-
-
-        const fetchDisplays = async () => {
-            const response = await fetch('http://localhost:8080/display/all');
-            const data = await response.json();
-            setDisplays(data); // Update the state with fetched data
-            const savedSelections = JSON.parse(localStorage.getItem("selectedMACs") || "[]");
-            setSelectedMACs(savedSelections);
-        };
-
-        fetchDisplays();
-
-        if (hasFetched.current) return
-        hasFetched.current = true
-
-        updateEvents()
-            .then(() => console.log('Events updated.'))
-            .catch(err => console.error(err))
-    }, []);
-
-    // Gefilterte Displays (nur aktivierte MACs)
-    const filteredDisplays = displays.filter(display => selectedMACs.includes(display.macAddress));
-
-    // Toggle Checkbox
-    const handleCheckboxChange = (mac: string) => {
-        let updatedSelections;
-        if (selectedMACs.includes(mac)) {
-            updatedSelections = selectedMACs.filter(item => item !== mac);
-        } else {
-            updatedSelections = [...selectedMACs, mac];
-        }
-
-        setSelectedMACs(updatedSelections);
-        localStorage.setItem("selectedMACs", JSON.stringify(updatedSelections));
-        // Kalendereinträge aktualisieren
-        updateEvents();
-    };
-
-    // Alle Displays auswählen/abwählen
-    const handleSelectAllChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const isChecked = event.target.checked;
-        const allMACs = displays.map(display => display.macAddress);
-
-        if (isChecked) {
-            setSelectedMACs(allMACs);
-        } else {
-            setSelectedMACs([]);
-        }
-
-        localStorage.setItem("selectedMACs", JSON.stringify(isChecked ? allMACs : []));
-        // Kalendereinträge aktualisieren
-        updateEvents();
-    };
-
-
+    const selectedMacsChanged = async (newSelectedMacs: string[]) => {
+        setSelectedMACs(newSelectedMacs)
+        setSelectedDisplaysOnStorage(newSelectedMacs)
+        await updateEvents()
+    }
 
     const closeCalendarEditEntryHandler = () => {
         setOpenCalendarEntry(false)
@@ -133,9 +82,7 @@ export default function Calendar() {
     }
 
     const handleDateClicked = (info: DateClickArg) => {
-        //const dateStr = formatDateToString(info.date, info.allDay)
-        let dateStr = formatDatetimeLocal(info.date , info.allDay)
-
+        const dateStr = formatDatetimeLocal(info.date , info.allDay)
         let start: string, end: string
 
         if (info.allDay) {
@@ -166,41 +113,24 @@ export default function Calendar() {
         setOpenCalendarEntry(true)
     }
 
-
-
     const formatDatetimeLocal = (date: Date, allday: boolean) => {
-
-
-        // Extrahiere Jahr, Monat, Tag, Stunde und Minute im richtigen Format
         const year = date.getFullYear();
         const month = String(date.getMonth() + 1).padStart(2, '0'); // Monat muss 2-stellig sein
         const day = String(date.getDate()).padStart(2, '0'); // Tag muss 2-stellig sein
         const hours = String(date.getHours()).padStart(2, '0'); // Stunden 2-stellig
         const minutes = String(date.getMinutes()).padStart(2, '0'); // Minuten 2-stellig
 
-        if(allday == false){
-            return `${year}-${month}-${day}T${hours}:${minutes}`;
-        } else {
-            return `${year}-${month}-${day}`;
-        }
+        return allday
+            ? `${year}-${month}-${day}`
+            : `${year}-${month}-${day}T${hours}:${minutes}`
     };
 
-
-
     const handleEventClicked = (evt: EventClickArg) => {
-        //const start = evt.event.start ? formatDateToString(evt.event.start, evt.event.allDay) : ""
-        //const end = evt.event.end ? formatDateToString(evt.event.end, evt.event.allDay) : start
-        let start = formatDatetimeLocal(evt.event.start ? evt.event.start : new Date(), evt.event.allDay)
-        let end
-        if (evt.event.end){
-            end = formatDatetimeLocal(evt.event.end, evt.event.allDay)
-        } else {
-            end = start
-        }
-        //let end = formatDatetimeLocal(evt.event.end ? evt.event.end : start, evt.event.allDay)
-        console.log("start: "+ start)
-        console.log("start_test: "+ start)
-        console.log("end: " + end)
+        const start = formatDatetimeLocal(evt.event.start ? evt.event.start : new Date(), evt.event.allDay)
+        const end = evt.event.end
+            ? formatDatetimeLocal(evt.event.end, evt.event.allDay)
+            : start
+
         const event: EventDetails = {
             id: evt.event.id,
             title: evt.event.title,
@@ -231,50 +161,9 @@ export default function Calendar() {
     return (
         <>
             <PageHeader title={'Kalender'} info={''}></PageHeader>
-            <div style={{ display: 'flex' }}>
-                {/* Left Column for Display Checkboxes */}
-                <div style={{width: '300px', paddingRight: '20px'}}>
-                    <h2 style={{fontSize: '24px', fontWeight: 'bold'}}>Display Filter</h2>
-                    <div>
-                        {/* Checkbox zum Auswählen/Auszuwählen aller Displays */}
-                        <label>
-                            <input
-                                type="checkbox"
-                                checked={selectedMACs.length === displays.length} // Wenn alle Displays ausgewählt sind
-                                onChange={handleSelectAllChange}
-                                style={{marginRight: '10px'}} // Abstand zwischen Checkbox und Text
-                            />
-                            Alle auswählen/abwählen
-                        </label>
-                    </div>
-                    <div style={{ borderBottom: '1px solid #ccc', margin: '10px 0' }}></div> {/* Trennstrich */}
-
-                    <ul>
-                        {displays.map(display => (
-                            <li key={display.id}>
-                                <label>
-                                    <input
-                                        type="checkbox"
-                                        checked={selectedMACs.includes(display.macAddress)}
-                                        onChange={() => handleCheckboxChange(display.macAddress)}
-                                        style={{marginRight: '10px'}} // Abstand zwischen Checkbox und Text
-                                    />
-                                    {"Display Id: " + display.id} ({"Mac: " + display.macAddress})
-                                </label>
-                            </li>
-                        ))}
-                    </ul>
-
-                    <h2>Gefilterte Displays</h2>
-                    <ul>
-                        {filteredDisplays.map(display => (
-                            <li key={display.id}>{display.id} ({display.macAddress})</li>
-                        ))}
-                    </ul>
-                </div>
-
-                {/* Right Column for Calendar */}
-                <div style={{ flex: 1 }}>
+            <div className={'flex gap-7'}>
+                <DisplayFilters selectedMacs={selectedMACs} onSelectedMacsChanged={selectedMacsChanged} />
+                <div className={'flex-1'}>
                     <FullCalendar
                         plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
                         events={events.map(evt => ({
---
+++ src/frontend/src/components/calendar/DisplayFilters.tsx
@@ -0,0 +1,67 @@
+import {DisplayData} from "@/types/displayData";
+import React, {useEffect, useRef, useState} from "react";
+import {Checkbox, Typography} from "@material-tailwind/react";
+
+type DisplayFiltersProps = {
+    selectedMacs: string[]
+    onSelectedMacsChanged: (macs: string[]) => void
+}
+
+export default function DisplayFilters({selectedMacs, onSelectedMacsChanged}: DisplayFiltersProps) {
+
+    const backendApiUrl = 'http://localhost:8080'
+
+    const [displays, setDisplays] = useState<DisplayData[]>([])
+    const hasFetched = useRef(false)
+
+    useEffect(() => {
+        if (hasFetched.current) return
+        hasFetched.current = true
+
+        updateDisplays()
+            .catch(err => console.error(err))
+    }, [])
+
+    const updateDisplays = async () => {
+        const response = await fetch(backendApiUrl + '/display/all')
+        setDisplays(await response.json() as DisplayData[])
+    }
+
+    const selectAllHandler = (evt: React.ChangeEvent<HTMLInputElement>) => {
+        const isChecked = evt.target.checked
+        if (isChecked) {
+            const allMacs = displays.map(d => d.macAddress)
+            onSelectedMacsChanged(allMacs)
+        } else {
+            onSelectedMacsChanged([])
+        }
+    }
+
+    const selectHandler = (macAddress: string) => {
+        const newSelectedMacs = selectedMacs.includes(macAddress)
+            ? selectedMacs.filter(m => m !== macAddress)
+            : [...selectedMacs, macAddress]
+        onSelectedMacsChanged(newSelectedMacs)
+    }
+
+    return (
+        <div className={'mt-[4rem]'}>
+            <h2 className={'text-lg font-bold'}>Display Filter</h2>
+            <Checkbox label={'Alle auswählen'} checked={selectedMacs.length === displays.length}
+                      onChange={selectAllHandler}/>
+            <hr className={'my-1 border-gray-400'} />
+            <div className={'flex flex-col'}>
+                {displays.map((d, index) =>
+                    <Checkbox key={index}
+                              label={
+                                  <div>
+                                      <Typography color={'blue-gray'} className={'leading-none'}>Display {d.id}</Typography>
+                                      <Typography className={'text-xs'} color={'gray'} >{d.macAddress}</Typography>
+                                  </div>
+                              }
+                              checked={selectedMacs.includes(d.macAddress)} className={'!p-0'}
+                              onChange={() => selectHandler(d.macAddress)} /> )}
+            </div>
+        </div>
+    )
+}
