6b52c4ee848f5581607acb9b772a9d99db521c5d
MikWink <winklermi76145@th-nuernberg.de>
2025-03-04 13:00:25 +0100

    Deleted artifact of recurrent events in event controller

--- src/main/java/master/it_projekt_tablohm/controller/EventController.java
+++ src/main/java/master/it_projekt_tablohm/controller/EventController.java
@@ -27,30 +27,6 @@ public class EventController {
         this.displayRepository = displayRepository;
     }
 
-    @CrossOrigin(origins = "*")
-    @GetMapping("/test")
-    public ResponseEntity<DateTime[]> testRRule() throws InvalidRecurrenceRuleException {
-        RecurrenceRule rule = new RecurrenceRule("FREQ=YEARLY;BYMONTHDAY=23;BYMONTH=5");
-
-        DateTime start = new DateTime(1982, 4 /* 0-based month numbers! */,23);
-
-        RecurrenceRuleIterator it = rule.iterator(start);
-
-        int maxInstances = 100; // limit instances for rules that recur forever
-
-        DateTime[] events = new DateTime[maxInstances];
-        int counter = 0;
-        while (it.hasNext() && (!rule.isInfinite() || maxInstances-- > 0))
-        {
-
-            DateTime nextInstance = it.nextDateTime();
-            // do something with nextInstance
-            events[counter] = nextInstance;
-            counter++;
-
-        }
-        return ResponseEntity.ok(events);
-    }
 
     @CrossOrigin(origins = "*")
     @PostMapping("/add")
