44277deea841faffa067f1fa3dd1bd2618ed5840
markus <markus@iMac-Pro.fritz.box>
2025-03-06 11:08:53 +0100

    - did add a fallback option for the backend api in the case that window is not defined (the case when code is not executed on the client, sometimes it seems to be the case event though "use client" is specified)

--- src/frontend/src/utils/backendApiUrl.ts
+++ src/frontend/src/utils/backendApiUrl.ts
@@ -1,5 +1,23 @@
+'use client'
 // src/utils/backendApiUrl.ts
+
 export const getBackendApiUrl = (): string => {
-    const host = window.location.hostname;
-    return `http://${host}:8080`;
+    if (typeof window !== "undefined") {
+        const host = window.location.hostname;
+        return `http://${host}:8080`;
+    } else {
+        // Fallback-Wert, wenn window nicht definiert ist
+        return "http://default-host:8080";
+    }
 };
+// src/utils/backendApiUrl.ts
+
+
+
+// export const getBackendApiUrl = (): string => {
+//     // Diese Funktion geht davon aus, dass sie nur im Client verwendet wird.
+//     if (typeof window === "undefined") {
+//         throw new Error("getBackendApiUrl darf nur im Client verwendet werden.");
+//     }
+//     return `http://${window.location.hostname}:8080`;
+// };
