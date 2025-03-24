ae30081e9547d2e4ddd0320b92f11fc14847235b
markus <markus@e53aa798-777f-4fa8-8b3e-df9e7cad7264.fritz.box>
2025-03-24 11:43:11 +0100

    did some cleanup and removed unnecessary files.

--- src/frontend/src/app/displays-server/loading.tsx
+++
@@ -1,4 +0,0 @@
-// next js will automatically use this file (name convention loading.tsx) to show the loading state for the server component defined in page.tsx
-export default function Loading(){
-    return <div>Loading Users...</div>;
-}
